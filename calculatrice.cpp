a82af4034fc3c2701d661e59b19610a4a0a7cc37
yuntux <yuntux@gmail.com>
2012-06-07 04:51:56 +0200

    Singleton Calculatrice

--- calculatrice.cpp
+++ calculatrice.cpp
@@ -1,5 +1,17 @@
 #include "calculatrice.h"
 
-Calculatrice::Calculatrice()
-{
+Calculatrice* Calculatrice::instance=0;
+
+Calculatrice::~Calculatrice(){
+}
+
+Calculatrice::Calculatrice(){}
+
+Calculatrice& Calculatrice::getInstance(){
+    if (!instance) instance= new Calculatrice();
+    return *instance;
+}
+
+void Calculatrice::libereInstance(){
+    delete instance;
 }
--- calculatrice.h
+++ calculatrice.h
@@ -1,10 +1,40 @@
 #ifndef CALCULATRICE_H
 #define CALCULATRICE_H
 
+#include "pile.h"
+#include "constante.h"
+#include "mainwindow.h"
+
 class Calculatrice
 {
+    static Calculatrice* instance;
+
+    enum MesureAngle _modAngle;
+    enum Type _modConstante;
+    bool _modComplexe;
+
+    Pile _pileStockage;
+    Pile _pileAffichage;
+
+    Pile* getPileStockage() {return &_pileStockage;}
+    Pile* getPileAffichage() {return &_pileAffichage;}
+    enum MesureAngle getMesureAngle() const {return _modAngle;}
+    enum Type getModConstante() const {return _modConstante;}
+    bool getModComplexe() const {return _modComplexe;}
+    void setModConstante(enum Type t){_modConstante = t;}
+    void setMesureAngle(enum MesureAngle a){_modAngle = a;}
+    void setModComplexe(bool c){_modComplexe = c;}
+    //void saveToFile();
+    //void loadFromFile();
+
+protected:
+    Calculatrice();  // à mettre dans la partie privée pour en interdire l'utilisationv
+    Calculatrice(const Calculatrice&);  // non défini mais à mettre dans la partie privée pour en interdire l'utilisation
+    virtual ~Calculatrice(); // à mettre dans la partie privée pour en interdire l'utilisationv
+    void operator=(const Calculatrice &);  // non défini mais à mettre dans la partie privée pour en interdire l'utilisation
 public:
-    Calculatrice();
+    static Calculatrice& getInstance();
+    static void libereInstance();
 };
 
 #endif // CALCULATRICE_H
